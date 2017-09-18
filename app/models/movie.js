f18d6dc62808625e972acc782f3eb8ddb29dd69e
Rick Twohy <twohyjr@gmail.com>
2017-09-18 12:18:31 -0500

    Create Movie Model

---
+++ app/models/movie.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+const{
+    attr
+}
+
+export default DS.Model.extend({
+    title: attr(),
+    vote_average: attr(),
+    vote_count: attr(),
+    video: attr(),
+    popularity: attr(),
+    poster_path: attr(),
+    original_language: attr(),
+    original_title: attr(),
+    backdrop_path: attr(),
+    adult: attr(),
+    overview: attr(),
+    release_date: attr()
+});
